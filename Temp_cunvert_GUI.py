6af6ae559215028e61500799c73e0d3d974c246d
Glitch-weasel <ryleyswatton@gmail.com>
2026-06-10 10:26:54 +1200

    v0.2
    2nd version

--- Temp_cunvert_GUI.py
+++ Temp_cunvert_GUI.py
@@ -1,10 +1,6 @@
 from tkinter import *
 
 
-font_Main_Title = "Veranda 16 bold"
-font_Heading = "Veranda 12 bold"
-font_Default = "Veranda 12"
-
 class TemperatureConverter:
 
     def F_to_C(self, temp):
@@ -12,13 +8,5 @@ class TemperatureConverter:
         if temp >= -273.15:
             result = (float(temp)-32) * 5 / 9
             return f'{result:.1f} degrees Centigrade'
-        else:
-            return "temperature too low"
-        
-    def C_to_f():
-        temp = float(temp)
-        if temp >= -459.67:
-            result = (float(temp)+32) / 5 * 9
-            return f'{result:.1f} degrees Fahrenheit'
         else:
             return "temperature too low"
